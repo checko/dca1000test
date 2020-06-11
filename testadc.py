62590b1019492101189f1c9a45fe6b8fb4ff0fd2
charles.chang <charles.chang@royaltek.com>
2020-06-11 11:19:15 +0800

    test save frame data in file

--- testadc.py
+++ testadc.py
@@ -1,8 +1,10 @@
-from adc import *
+from adc import DCA1000
+import numpy as np
 
 dca = DCA1000()
 dca.send_start_command()
 adc_data = dca.read(timeout=.1)
+np.save('framedata',adc_data)
 frame = dca.organize(adc_data,num_chirps=16,num_rx=4,num_samples=256)
 dca.send_stop_command()
 #print(frame[0][0][0])
