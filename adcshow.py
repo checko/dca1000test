f68dae5583b33bc04aa73a44fba2439e90e199df
charles.chang <charles.chang@royaltek.com>
2020-06-16 11:16:11 +0800

    show range-doppler fft

--- adcshow.py
+++ adcshow.py
@@ -6,7 +6,7 @@ dca = DCA1000()
 dca.connect_command()
 
 
-for bigi in range(20):
+for bigi in range(40):
     dca.send_start_command()
     adc_data = dca.read(timeout=.1)
     dca.send_stop_command()
@@ -21,11 +21,9 @@ for bigi in range(20):
     plt.pause(0.01)
     '''
     range_plot = np.fft.fft(frame,axis=1)
-    plt.imshow(np.abs(range_plot))
-    plt.pause(0.1)
+    #plt.imshow(np.abs(range_plot))
+    #plt.pause(0.1)
 
-'''
     range_doppler = np.fft.fft(range_plot,axis=0)
     plt.imshow(np.abs(range_doppler))
-    plt.pause(1)
-'''
+    plt.pause(0.05)
